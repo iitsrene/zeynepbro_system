d6be47c2e0715bd03e82e5183f5a8bebbaa6f7e8
iitsrene <rene-dc@outlook.de>
2026-07-13 00:12:58 +0200

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "KIdd Kawaki - HOODIE",
+        name: "ZEYNEPBRRO - lifestyle",
         // Activity type number (0 = Playing).
         type: 2,
       },
